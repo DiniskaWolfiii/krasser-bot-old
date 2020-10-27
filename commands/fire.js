5d7487d141dc88e557ecc680900ab3f0db4f8e56
DiniskaWolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2020-10-27 21:59:41 +0100

    wegaw

---
+++ commands/fire.js
@@ -0,0 +1,28 @@
+/**
+ *
+ *
+ * @param {import('discord.js').Client} client
+ * @param {import('discord.js').Message} message
+ */
+module.exports.run = async (client, message, args) => {
+    let messageUser = message.member;
+    let taggedUser = message.mentions.members.first();
+
+    try {
+        let antworten = [
+            `*${messageUser} zündet ${taggedUser} an :fire:*`
+        ]
+        let randomNumber = Math.floor(Math.random()*antworten.length);
+        message.delete();
+        message.channel.send(antworten[randomNumber]);
+    } catch (error) {
+        message.channel.send("```js\n" + error + "\n```");
+    }
+
+}
+
+module.exports.help = {
+    name: 'fire',
+    usage: '!fire [@User]',
+    description: 'Zündet einen User an'
+}
