05b8b2718cbce02b79ac26407142ddac5d9d5a4c
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2020-11-13 10:38:10 +0100

    Update knife.js

--- commands/knife.js
+++ commands/knife.js
@@ -1,1 +1,39 @@
+/**
+ *
+ *
+ * @param {import('discord.js').Client} client
+ * @param {import('discord.js').Message} message
+ */
+module.exports.run = async (client, message, args) => {
+    let messageUser = message.member;
+    let taggedUser = message.mentions.members.first();
+    if(!taggedUser) {
+        message.delete();
+        let antworten = [
+          `*${messageUser} hält energisch ein Messer in der Hand*`,
+          `*${messageUser} hält energisch ein Messer in der Hand und beobachtet jemanden :eyes:*`
+        ]
+        let randomNumber = Math.floor(Math.random()*antworten.length);
+        return message.channel.send(antworten[randomNumber]);
+    }
+    try {
+        let antworten = [
+            `*${messageUser} wirft ${taggedUser} mit einem Messer ab!*`,
+            `*${messageUser} wirft ${taggedUser} mit einem Messer ab!*`,
+            `*${messageUser} wirft ${taggedUser} mit einem Messer ab!*`,
+            `*${messageUser} wirft ${taggedUser} mit einem Messer ab und nagelt ${taggedUser} damit an die Wand!*`
+        ]
+        let randomNumber = Math.floor(Math.random()*antworten.length);
+        message.delete();
+        message.channel.send(antworten[randomNumber]);
+    } catch (error) {
+        message.channel.send("```js\n" + error + "\n```");
+    }
 
+}
+
+module.exports.help = {
+    name: 'knife',
+    usage: '!knife <@User>',
+    description: 'Wirf mit Messern nach Leuten. Hehe'
+}
