9e356aa76d9dc0d90763576fefaaa78999808f06
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2020-12-03 21:44:59 +0100

    Create stab.js

---
+++ commands/stab.js
@@ -0,0 +1,39 @@
+/**
+ *
+ *
+ * @param {import('discord.js').Client} client
+ * @param {import('discord.js').Message} message
+ */
+module.exports.run = async (client, message, args) => {
+    let messageUser = message.member;
+    let taggedUser = message.mentions.members.first();
+    if(!taggedUser) {
+        message.delete();
+        let antworten = [
+          `*${messageUser} hat das Verlangen jemand abzustechen :knife:*`,
+          `*${messageUser} hebt ein Messer hervor :knife:*`
+        ]
+        let randomNumber = Math.floor(Math.random()*antworten.length);
+        return message.channel.send(antworten[randomNumber]);
+    }
+    try {
+        let antworten = [
+            `*${messageUser} sticht ${taggedUser} ab :knife:*`,
+            `*${messageUser} sticht ${taggedUser} ab :knife:*`,
+            `*${messageUser} sticht ${taggedUser} ab :knife:*`,
+            `*${messageUser} sticht ${taggedUser} ab :knife:*`
+        ]
+        let randomNumber = Math.floor(Math.random()*antworten.length);
+        message.delete();
+        message.channel.send(antworten[randomNumber]);
+    } catch (error) {
+        message.channel.send("```js\n" + error + "\n```");
+    }
+
+}
+
+module.exports.help = {
+    name: 'slap',
+    usage: '!slap <@User>',
+    description: 'Klatsche einem Typen'
+}
