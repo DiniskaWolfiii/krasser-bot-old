8392027ad5ec0dc3d3718bf2dd69d07d0efed384
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2021-04-22 12:21:34 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,1 +1,26 @@
 
+/**
+ *
+ *
+ * @param {import('discord.js').Client} client
+ * @param {import('discord.js').Message} message
+ */
+module.exports.run = async (client, message, args) => {
+    let text = '';
+    try {
+        for(var i = 0; i < args.length; i++) {
+text = text + args[i];
+    }
+message.delete();
+message.channel.send(text);
+    catch (error) {
+        message.channel.send("```js\n" + error + "\n```");
+    }
+
+}
+
+module.exports.help = {
+    name: 'say',
+    usage: '!say <Text>',
+    description: 'Wiederholt was du sagst.'
+}
