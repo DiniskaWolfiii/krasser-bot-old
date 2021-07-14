75a599ddea008e2485051c03133476d4ccadb2b4
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2021-07-14 23:29:25 +0200

    added an embed or something dunno

--- commands/thema.js
+++ commands/thema.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 /**
  *
  *
@@ -5,19 +7,33 @@
  * @param {import('discord.js').Message} message
  */
 module.exports.run = (client, message, args) => {
-    if (message.channel.topic.startsWith('AKTIV:')) return message.channel.send('Es ist gerade ein Thema am laufen! Check die Channel Beschreibung!');
+    if (message.channel.topic.startsWith('AKTIV:')) {
+        let txtEmbed = new Discord.MessageEmbed()
+            .setDescription('Es ist gerade ein Thema zum Diskutieren am laufen! Checke die Channel Beschreibung!')
+            .setThumbnail(client.user.displayAvatarURL())
+            .setColor('585858')
+        return message.channel.send(txtEmbed);
+    }
     let thema = '';
     for (let i = 0; i < args.length; i++) {
         thema = thema + args[i] + ' ';
     }
-    let themenKanal = message.guild.channels.cache.findKey(c=>c.id==='864914449906794556')
-    if(!themenKanal) return message.delete();
+    let themenKanal = message.guild.channels.cache.findKey(c => c.id === '864914449906794556')
+    if (!themenKanal) return message.delete();
     try {
+        let txtEmbed = new Discord.MessageEmbed()
+            .setDescription(`Alles klar! Das neue Thema zum diskutieren lautet: ${thema}. 2 Stunden laufen ab... JETZT!`)
+            .setThumbnail(client.user.displayAvatarURL())
+            .setColor('585858')
         message.channel.setTopic(`AKTIV: ` + thema)
-            .then(newChannel => message.channel.send(`Alles klar! Neues Thema zum diskutieren lautet: ${thema}. 2 Stunden laufen JETZT!`))
+            .then(newChannel => message.channel.send(txtEmbed))
             .catch(console.error);
         setTimeout(() => {
-            message.channel.send('Thema zum diskutieren vorbei! Jetzt kann ein neues Thema mit !thema ausgesucht werden');
+            let vorbeiEmbed = new Discord.MessageEmbed()
+                .setDescription(`Thema zum diskutieren vorbei! Jetzt kann mit !thema ein neues Thema zum diskutieren gewählt werden!`)
+                .setThumbnail(client.user.displayAvatarURL())
+                .setColor('585858')
+            message.channel.send(vorbeiEmbed);
             message.channel.setTopic('FREI. Starte ein neues Thema mit !thema')
         }, 7200000);
     }
